3bcd9015f8c9ecc65a396974956fb362a0aa6012
young <343284587@qq.com>
2019-05-26 19:23:49 +0800

    项目初始化

---
+++ meiduo_mall_admin/src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>单文件组件：{{ content }}</div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      content: 'hello!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
